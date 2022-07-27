e65b3f7a9d4202c2cf05d723701c75ae36971216
Sergio Medina <sme@monsido.com>
2022-07-27 12:12:06 +0200

    cleanup and remove warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require 'piwik'
 
+require 'rspec/its'
+
 RSpec.configure do |config|
   Dir[File.join(File.dirname(__FILE__),'spec','support''**','*.rb')].each {|f| require f}
   config.mock_with :rspec
